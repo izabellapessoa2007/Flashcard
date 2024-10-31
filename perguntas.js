41f4912f3c150f54b98e5ee9f1021dc27afe48e5
izabellapessoa2007 <00001121583416sp@al.educacao.sp.gov.br>
2024-10-31 17:09:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Curiosidade',
+    'O que é mais pesado: 1 quilo de algodão ou 1 quilo de ferro?',
+    'Ambos têm o mesmo peso: 1 quilo. Mas, como precisamos de uma porção de algodão maior para compor 1 quilo, temos a sensação de que o mesmo peso de algodão é mais leve do que o chumbo.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do mundo',
+    'É o Monte Everest'
+)
+
+criaCartao(
+    'Ciência',
+    'Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?',
+    'A Terra demora aproximadamente 24 horas, mais precisamente 23 horas, 56 minutos e 4 segundos para dar uma volta completa em torno do seu próprio eixo. Esse movimento recebe o nome de rotação.'
+)
+
+criaCartao(
+    'Geral',
+    ' O que representam os cinco anéis olímpicos?',
+    'A bandeira olímpica é branca e contém cinco anéis, cada um com uma cor diferente. Esses anéis são representados nas seguintes posições: na parte de cima - da esquerda para a direita - estão os anéis azul, preto e vermelho; na parte de baixo - da esquerda para a direita - estão os anéis amarelo e verde.'
+)
+
+criaCartao(
+    'Ciência',
+    'Quantos ossos temos no nosso corpo?',
+    'O corpo humano tem 206 ossos: 22 na cabeça, 1 no pescoço, 6 no ouvido, 44 no tórax, 7 no abdômen, 62 nos membros inferiores e 64 nos membros inferiores.'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é maior: gigabyte ou megabyte?',
+    'O gigabyte é maior do que o megabyte. Um gigabyte equivale a 1024 megabytes.'
+)
+
+
+
+criaCartao(
+    'História',
+    'Que grande evento histórico aconteceu em 1822 no Brasil?',
+    'Em 1822, Dom Pedro proclamou a independência do Brasil no dia 7 de setembro, data que é feriado nacional..'
+)
+
+
+
+criaCartao(
+    'Contos',
+    'Em que conto de fadas uma princesa adormece com um feitiço e só acorda com o beijo de um príncipe?',
+    'A Bela Adormecida conta a história de uma bebê que foi amaldiçoada a cair num sono profundo se espetasse o seu dedo no fuso de uma roca, o que aconteceu quando ela tinha 16 anos. O feitiço foi desfeito depois de o príncipe a beijar e, assim, conseguir despertá-la..'
+)
+
+
+
+criaCartao(
+    'História',
+    'Que navio famoso naufragou na sua viagem inaugural?',
+    'O Titanic naufragou em abril de 1912, quatro dias depois de ter iniciado a sua primeira viagem, que saiu da Inglaterra com destino aos Estados Unidos.'
+)
